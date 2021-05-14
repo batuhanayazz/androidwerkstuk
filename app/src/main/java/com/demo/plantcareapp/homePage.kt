f0a37edc57ca21a126095b5b2ca2ad0c43ae41b7
batuhanayazz <43605933+batuhanayazz@users.noreply.github.com>
2021-05-14 13:42:31 +0200

    Project Started, Working on GUI

---
+++ app/src/main/java/com/demo/plantcareapp/homePage.kt
@@ -0,0 +1,11 @@
+package com.demo.plantcareapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class homePage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home_page)
+    }
+}
